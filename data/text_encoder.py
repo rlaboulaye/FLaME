e118bc6ee762e85b2a1d3dc85720c83faa442ab0
Roland Laboulaye <rlaboulaye@gmail.com>
2019-02-26 14:18:00 -0700

    renamed token

--- data/text_encoder.py
+++ data/text_encoder.py
@@ -12,13 +12,13 @@ class TextEncoder(object):
         self.encoder = json.load(open(encoder_path))
         self.start_token_ = '_start_'
         self.delimeter_token_ = '_delimiter_'
-        self.classify_token_ = '_classify_'
+        self.end_token_ = '_end_'
         self.encoder[self.start_token_] = len(self.encoder)
         self.encoder[self.delimeter_token_] = len(self.encoder)
-        self.encoder[self.classify_token_] = len(self.encoder)
+        self.encoder[self.end_token_] = len(self.encoder)
         self.start_token = self.encoder[self.start_token_]
         self.delimeter_token = self.encoder[self.delimeter_token_]
-        self.classify_token = self.encoder[self.classify_token_]
+        self.end_token = self.encoder[self.end_token_]
         self.decoder = {v:k for k,v in self.encoder.items()}
         merges = open(bpe_path, encoding='utf-8').read().split('\n')[1:-1]
         merges = [tuple(merge.split()) for merge in merges]
