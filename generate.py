9c998f9d0360659406e763bc5f699dd122d03364
Roland Laboulaye <rlaboulaye@gmail.com>
2019-03-10 03:03:29 -0600

    changed params for generation

--- generate.py
+++ generate.py
@@ -19,7 +19,7 @@ def encode_prime(text_encoder, prime, sequence_dim):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--hyperparams', type=str, default='hyperparams/pretrain.json')
-    parser.add_argument('--params', type=str, default='params/language_modeling_2019-03-06 17:31:11')
+    parser.add_argument('--params', type=str, default='params/language_modeling__6_to_11_len_books_in_sentences_2019-03-08 18:45:14')
     parser.add_argument('--prime', type=str, default='')
     parser.add_argument('--n_iter', type=int, default=10)
 
